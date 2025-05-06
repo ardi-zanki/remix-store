0de219b0959f45d2830798eb75a702285c5a1ffa
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-06 15:56:15 -0500

    Add "load more" logic (#149)
    * Refactor routes
    
    * Add load-more-products resource
    
    * Change actions to resources
    
    * Increase the number of products loaded in collections from 8 to 15

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,15 @@
 import { flatRoutes } from "@remix-run/fs-routes";
 import { hydrogenRoutes } from "@shopify/hydrogen";
-import { type RouteConfig } from "@remix-run/route-config";
+import { prefix, route, type RouteConfig } from "@remix-run/route-config";
 
-const routes = hydrogenRoutes(await flatRoutes()) satisfies RouteConfig;
+const routes = [
+  ...(await hydrogenRoutes(
+    await flatRoutes({ rootDirectory: "routes/pages" }),
+  )),
+  ...prefix("_resources", [
+    route("color-scheme", "routes/resources/color-scheme.tsx"),
+    route("load-more-products", "routes/resources/load-more-products.tsx"),
+  ]),
+] satisfies RouteConfig;
 
 export default routes;
