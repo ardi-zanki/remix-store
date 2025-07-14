1ada5a28169ed0c3323f8639cc03b7c092cea85b
Brooks Lybrand <brookslybrand@gmail.com>
2025-07-14 15:14:31 -0500

    Remove 7-10 business day shipping notice on PDP page

--- app/routes/pages/($locale).products.$handle.tsx
+++ app/routes/pages/($locale).products.$handle.tsx
@@ -177,31 +177,6 @@ function ProductMain({ product }: { product: ProductLoaderData }) {
             />
           </>
         ) : null}
-        {/* Temporary shipping information for store launch with delayed products */}
-        <>
-          <h3 className="text-sm font-bold lg:text-base">Shipping Info</h3>
-          <div className="rich-text text-xs lg:text-base">
-            <ul>
-              <li>Ready to ship in 7-10 business days</li>
-              <li>
-                Please note that items will be shipped together once all items
-                in the order are in-hand and ready to ship
-              </li>
-              <li>
-                For shipping questions, please contact{" "}
-                <a href="mailto:support@merch.co">support@merch.co</a> or
-                consult our{" "}
-                <Link
-                  to={href("/:locale?/policies/:handle", {
-                    handle: "shipping-policy",
-                  })}
-                >
-                  Shipping Policy
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </>
       </div>
       <Analytics.ProductView
         data={{
